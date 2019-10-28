a1f90d1fa71cf796ceaaaf9f1da90a1c6eb60c39
Guaratã Alencar <guara_st@gmail.com>
2019-10-28 16:58:47 -0300

    links

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,14 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/item',
+    name: 'item',
+    component: () => import(/* webpackChunkName: "item" */ '../views/Item.vue'),
+  },
+  {
+    path: '/new',
+    name: 'new',
+    component: () => import(/* webpackChunkName: "new" */ '../views/New.vue'),
   },
 ];
 
