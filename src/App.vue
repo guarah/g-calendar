c39fa7b6252f3e693f33dfbed918d622a28a5ec6
Guaratã Alencar <guara_st@gmail.com>
2019-10-29 10:35:48 -0300

    Basic layout & info structure

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@
 </template>
 
 <style lang="scss">
+body {
+  margin: 0px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/Item.vue
+++ src/views/Item.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
-    <h1>Item details</h1>
+    <!-- Nav bar -->
+    <div class="nav-bar">
+      <h2>Detalhes do Item</h2>
+    </div>
+    <div class="details">
+      <p>1</p>
+      <p>name</p>
+      <p>Age</p>
+      <p>3 4 6</p>
+      <p>other info</p>
+      <p>1 2 3 4</p>
+      <p>Date</p>
+      <p>28/11/2019</p>
+      <p>some extra detail</p>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.details {
+  padding-top: 75px;
+  padding-bottom: 65px;
+}
+</style>
